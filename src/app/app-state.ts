f9a018ae2dbb5d039df0711f372143c266539201
Z11430373 <11430373@mail.hpsh.tp.edu.tw>
2026-09-20 11:26:01 +0800

    Update Zustand store for snake_case app state and parse results

--- src/app/app-state.ts
+++ src/app/app-state.ts
@@ -1,24 +1,24 @@
 import { create } from 'zustand'
 
-import type { ASTNode, AppState, Camera } from './types'
+import type { AppState, Camera, ParseResult } from './types'
 
 interface AppActions {
   setLevel: (level: AppState['level']) => void
   setCamera: (camera: Camera) => void
   updateCamera: (partial: Partial<Camera>) => void
-  setSelectedCardId: (selectedCardId: string | null) => void
-  setSelectedElementId: (selectedElementId: string | null) => void
-  setMainZmd: (mainZmd: string) => void
-  setAst: (ast: ASTNode[]) => void
+  setSelectedCardId: (selected_card_id: string | null) => void
+  setSelectedElementId: (selected_element_id: string | null) => void
+  setMainZmd: (main_zmd: string) => void
+  setParseResult: (result: ParseResult | null) => void
 }
 
 export const initialAppState: AppState = {
   level: 1,
   camera: { x: 0, y: 0, zoom: 1 },
-  selectedCardId: null,
-  selectedElementId: null,
-  mainZmd: '',
-  ast: []
+  selected_card_id: null,
+  selected_element_id: null,
+  main_zmd: '',
+  parse_result: null
 }
 
 export const useAppStore = create<AppState & AppActions>((set) => ({
@@ -32,8 +32,8 @@ export const useAppStore = create<AppState & AppActions>((set) => ({
         ...partial
       }
     })),
-  setSelectedCardId: (selectedCardId) => set({ selectedCardId }),
-  setSelectedElementId: (selectedElementId) => set({ selectedElementId }),
-  setMainZmd: (mainZmd) => set({ mainZmd }),
-  setAst: (ast) => set({ ast })
+  setSelectedCardId: (selected_card_id) => set({ selected_card_id }),
+  setSelectedElementId: (selected_element_id) => set({ selected_element_id }),
+  setMainZmd: (main_zmd) => set({ main_zmd }),
+  setParseResult: (parse_result) => set({ parse_result })
 }))
